8ab5a4035d9d3e2a1c682e0f62d1c214133ef3ca
vvarvianskiy <54183515+vvarvianskiy@users.noreply.github.com>
2021-11-02 16:30:41 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-function animateScript() {
-  document.getElementById("image").style.backgroundPosition = 
-  `-256px 0px`;
-  }
+// function animateScript() {
+//   document.getElementById("image").style.backgroundPosition = 
+//   `-256px 0px`;
+//   }
+
+
+
+// var cartoon_background1=new cartoon_background();//создаю новый экземпляр обьекта, имя придумываю сам
+// cartoon_background1.play({
+// 	id:'cartoon',//id div элемента
+// 	src:'test_img/test.png',//ссылка на изображение
+// 	frames_x:1,//количество кадров на изображении по x
+// 	frames_y:75,//количество кадров на изображении по y
+// 	width:74325,//ширина изображения в пикселях
+// 	height:400,//высота изображения в пикселях
+// 	fps:24,//скорость показа кадров в секунду, можно писать дробные числа
+// 	reverse:false,//если true то проигрывается с конца в начало
+// 	playandstop:false,//если true то проиграть 1 раз и передать управление функции next
+// 	next: function () {//выполнится только если playandstop==true
+// 		//сюда можно пысать следующий код который будет выполняться после завершения анимации
+// 		//к примеру можно вставить следующую анимацию с другим изображением
+// 	}
+// });
